df2455361ad8fc07d4510d237f8a3245ca77a6fe
Patrik Bolander <patrik.bolander@datacom.co.nz>
2024-04-04 15:16:44 +1300

    finished chat header

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -8,7 +8,7 @@ const MainLayout = async ({ children }: { children: React.ReactNode }) => {
             <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                 <NavigationSideBar />
             </div>
-            <main className="md:pl-[72px] h-full">
+            <main className="md:pl-[72px] h-full w-full">
                 {children}
             </main>
         </div>
