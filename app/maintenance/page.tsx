698c371b5aa855c97fdf68f547fcfa5f5c62416e
PatrikBolanderDC <patrik.bolander@datacom.com>
2024-09-10 10:22:59 +1200

    wip maintenance page

--- app/maintenance/page.tsx
+++ app/maintenance/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {
   emojiCursor,
   ghostCursor,
@@ -21,55 +21,91 @@ interface CursorEffectConfig {
 }
 
 enum CursorEffectName {
-    Emoji = "Emoji",
-    Ghost = "Ghost",
-    Rainbow = "Rainbow",
-    FairyDust = "FairyDust",
-    Trailing = "Trailing",
-    TextFlag = "TextFlag",
-    Bubble = "Bubble",
-    Snowflake = "Snowflake",
-    Clock = "Clock",
-    Character = "Character"
-  }
+  Emoji = "Emoji",
+  Ghost = "Ghost",
+  Rainbow = "Rainbow",
+  FairyDust = "FairyDust",
+  Trailing = "Trailing",
+  TextFlag = "TextFlag",
+  Bubble = "Bubble",
+  Snowflake = "Snowflake",
+  Clock = "Clock",
+  Character = "Character",
+}
 
-
-
-const getCursorEffectConfig = (effectName: CursorEffectName): CursorEffectConfig | null => {
-    const cursorEffects: { [key in CursorEffectName]: CursorEffectConfig } = {
-      [CursorEffectName.Emoji]: { effect: emojiCursor, params: { emoji: ["🔥", "🐬", "🦆"] } },
-      [CursorEffectName.Ghost]: { effect: ghostCursor },
-      [CursorEffectName.Rainbow]: { effect: rainbowCursor },
-      [CursorEffectName.FairyDust]: { effect: fairyDustCursor },
-      [CursorEffectName.Trailing]: { effect: trailingCursor },
-      [CursorEffectName.TextFlag]: { effect: textFlag, params: { text: "Discord Clone", color: "gray", size: 20, font: "Arial", textSize: 60, gap: 40 } },
-      [CursorEffectName.Bubble]: { effect: bubbleCursor },
-      [CursorEffectName.Snowflake]: { effect: snowflakeCursor },
-      [CursorEffectName.Clock]: { effect: clockCursor, params: { dateColor: "red", faceColor: "blue", hoursColor: "green", minutesColor: "yellow", secondsColor: "purple" } },
-      [CursorEffectName.Character]: { effect: characterCursor, params: { characters: ["test", "test2"], colors: ["red", "green"] } }
-    };
-  
-    return cursorEffects[effectName] || null;
+const getCursorEffectConfig = (
+  effectName: CursorEffectName
+): CursorEffectConfig | null => {
+  const cursorEffects: { [key in CursorEffectName]: CursorEffectConfig } = {
+    [CursorEffectName.Emoji]: {
+      effect: emojiCursor,
+      params: { emoji: ["🔥", "🐬", "🦆"] },
+    },
+    [CursorEffectName.Ghost]: { effect: ghostCursor },
+    [CursorEffectName.Rainbow]: { effect: rainbowCursor },
+    [CursorEffectName.FairyDust]: { effect: fairyDustCursor },
+    [CursorEffectName.Trailing]: { effect: trailingCursor },
+    [CursorEffectName.TextFlag]: {
+      effect: textFlag,
+      params: {
+        text: "Discord Clone",
+        color: "gray",
+        size: 20,
+        font: "Arial",
+        textSize: 60,
+        gap: 40,
+      },
+    },
+    [CursorEffectName.Bubble]: { effect: bubbleCursor },
+    [CursorEffectName.Snowflake]: { effect: snowflakeCursor },
+    [CursorEffectName.Clock]: {
+      effect: clockCursor,
+      params: {
+        dateColor: "red",
+        faceColor: "blue",
+        hoursColor: "green",
+        minutesColor: "yellow",
+        secondsColor: "purple",
+      },
+    },
+    [CursorEffectName.Character]: {
+      effect: characterCursor,
+      params: { characters: ["test", "test2"], colors: ["red", "green"] },
+    },
   };
 
+  return cursorEffects[effectName] || null;
+};
+
+let CURSOR_EFFECT: any;
+
+// TODO: Fix the cursor effect. its adding every cursor effect at the same time
 const MaintenancePage = () => {
+  const [cursorEffectConfig, setCursorEffectConfig] = useState(getCursorEffectConfig(CursorEffectName.Emoji));
+  const [cursorEffect, setCursorEffect] = useState(cursorEffectConfig?.effect(cursorEffectConfig.params));
+
   const handleRefresh = () => {
     window.location.reload();
   };
 
-  useEffect(() => {
-    const cursorEffectConfig = getCursorEffectConfig(CursorEffectName.Ghost);
-    if (cursorEffectConfig) {
-      const cursorEffect = cursorEffectConfig.effect(cursorEffectConfig.params);
+  const CursorKeys = Object.keys(CursorEffectName).filter((v) =>
+    isNaN(Number(v))
+  );
 
-      return () => {
-        cursorEffect.destroy();
-      };
-    }
-  }, []);
+  const handleChangeCursor = (effectName: CursorEffectName) => {
+    setCursorEffectConfig(getCursorEffectConfig(CursorEffectName[effectName]));
+    setCursorEffect(cursorEffectConfig?.effect(cursorEffectConfig.params));
+  };
+
+  useEffect(() => {
+    CURSOR_EFFECT = cursorEffectConfig?.effect(cursorEffectConfig.params);
+    return () => {
+      CURSOR_EFFECT?.destroy();
+    };
+  }, [cursorEffect, cursorEffectConfig]);
 
   return (
-    <div className="flex flex-col h-screen items-center justify-center ">
+    <div className="container flex flex-col h-screen items-center justify-center">
       <Image
         src="/images/discord_logo.png"
         alt="Maintenance"
@@ -89,6 +125,23 @@ const MaintenancePage = () => {
         <Button onClick={handleRefresh} variant="primary">
           Refresh
         </Button>
+      </div>
+
+      <div className="w-3/4 flex flex-wrap justify-center items-center mt-10">
+        {CursorKeys.map((key) => {
+          const effectName =
+            CursorEffectName[key as keyof typeof CursorEffectName];
+          return (
+            <Button
+              key={effectName}
+              onClick={() => handleChangeCursor(effectName)}
+              variant="secondary"
+              className="mr-4 my-2 "
+            >
+              {effectName}
+            </Button>
+          );
+        })}
       </div>
     </div>
   );
